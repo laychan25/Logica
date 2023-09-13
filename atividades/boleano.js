6005e228ecdf2e6808df8419106b50213db9ac5f
laychan25 <lauriesilva0@gmail.com>
2023-09-13 18:57:13 -0300

    first commit

---
+++ atividades/boleano.js
@@ -0,0 +1,24 @@
+const numUm = 1;
+const stringUm = '1';
+const numTrinta = 30;
+const stringTrinta = '30';
+const numDez = 10;
+const stringDez = '10'
+
+if(numUm === stringUm){
+   console.log(`Sao iguais`)
+}else{
+   console.log(`Possuem o mesmo valor mas sao dados diferente`) 
+}
+
+if(numTrinta === stringTrinta){
+   console.log(`Sao iguais`)
+}else{ 
+   console.log(`Possuem o mesmo valor mas sao dados diferente`)
+}
+
+if( numDez === stringDez){
+    console.log(`sao iguais`)
+}else{
+    console.log(`Possuem o mesmo valor mas sao dados diferente`)
+}
